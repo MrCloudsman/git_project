486b45112cdf954995ad82ae738a6de6ffdfa799
liunan <1993483232@qq.com>
2019-01-04 17:36:07 +0800

    idea git project submit

---
+++ git_project/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+/**
+ * @Author: Mr.Liu
+ * @Date: 2019/1/4 17:28
+ * @Location: Shenzhen
+ * @Version: 1.1.0
+ */
+public class User {
+    private String name;
+    private int age;
+    private String address;
+}
